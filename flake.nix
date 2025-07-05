1b281881149b4edb789a8a117a700684ef73aa09
PJalv <pjalvbusiness@gmail.com>
2025-07-04 21:33:41 -0700

    tweak

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        targetKernel = pkgs.linuxPackages_6_13;
+        targetKernel = pkgs.linuxPackages_rpi4;
 
         # Generate compile_commands.json for the kernel
         # This will allow LSP servers to understand kernel code
@@ -39,30 +39,13 @@
             name = "kernel-dev-environment-with-lsp";
 
             buildInputs = with pkgs; [
-              # Basic development tools
-              gnumake
-              gcc
-              pkg-config
-
-              # Clang toolchain for LSP support
               clang
               clang-tools # Includes clangd
               llvm
               lldb
-
-              # LSP support tools
-              bear # For generating compile_commands.json
-              compiledb
-
-              # Kernel specific
-              targetKernel.kernel.dev
-
             ];
 
             shellHook = ''
-              export KERNELDIR="${targetKernel.kernel.dev}/lib/modules/${targetKernel.kernel.version}/build"
-              export KERNEL_VERSION="${targetKernel.kernel.version}"
-              export PROJECT_DIR="$PWD"
 
               # Generate .clangd configuration file for kernel development
               cat > .clangd << EOF
@@ -75,16 +58,6 @@
               Index:
                 Background: Build
               EOF
-
-              # Create symlink to kernel headers if not present
-              if [ ! -d "./kernel-headers" ]; then
-                ln -sf $KERNELDIR/include ./kernel-headers
-              fi
-
-              echo "Kernel development environment set up for Linux ${targetKernel.kernel.version}"
-              echo "LSP support is enabled with clangd"
-              echo "Run 'generate-compile-commands' to create compile_commands.json for LSP"
-              echo "Make sure your editor is configured to use clangd"
             '';
           };
         };
